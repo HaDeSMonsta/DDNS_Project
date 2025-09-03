74770a5dc5a6e735cfd0c9f6f36b5ae0d8d3cdad
HaDeSMonsta <leon.poellinger+github@gmail.com>
2025-09-03 09:19:40 +0200

    Finish moving, improve building

--- justfile
+++ justfile
@@ -1,28 +1,14 @@
 set dotenv-filename := "just.env"
 
 default:
-	@just --list
+    @just --list
 
 client:
-	cargo run --bin ddns_client
+    cargo run --bin ddns_client
 
 server:
-	cargo run --bin ddns_server
+    cargo run --bin ddns_server
 
 publish major minor patch:
-    docker logout
-    echo "$CR_PAT" | docker login ghcr.io -u "$CR_USERNAME" --password-stdin
-
-    docker build ./ddns_client/ -f ./ddns_client/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}.{{patch}}"
-
-    docker tag "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}.{{patch}}" "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}"
-    docker tag "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}.{{patch}}" "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}"
-    docker tag "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}.{{patch}}" "ghcr.io/hadesmonsta/ddns_project:client-latest"
-
-    docker push "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}.{{patch}}"
-    docker push "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}.{{minor}}"
-    docker push "ghcr.io/hadesmonsta/ddns_project:client-v{{major}}"
-    docker push "ghcr.io/hadesmonsta/ddns_project:client-latest"
-
-    docker logout
-    ./release_server.sh {{major}} {{minor}} {{patch}}
+    mprocs --config mp_build.yaml
+    ./release_server.sh {{ major }} {{ minor }} {{ patch }}
--- release_server.sh
+++ release_server.sh
@@ -4,22 +4,39 @@ set -euxo pipefail
 major="$1"
 minor="$2"
 patch="$3"
-full="${major}.${minor}.${patch}"
 
 docker logout
 echo "$CR_PAT" | docker login ghcr.io -u "$CR_USERNAME" --password-stdin
 
-docker build ./ddns_server/ -f ddns_server/docker/default/release/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:server-v${full}"
-docker build ./ddns_server/ -f ddns_server/docker/nc/release/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:server-netcup-v${full}"
+docker build ./ -f docker/client/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:client-latest"
+docker build ./ -f docker/server/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:server-latest"
+docker build ./ -f docker/server/netcup/Dockerfile -t "ghcr.io/hadesmonsta/ddns_project:server-netcup-latest"
+
+##############
+### CLIENT ###
+##############
+
+docker tag "ghcr.io/hadesmonsta/ddns_project:client-latest" "ghcr.io/hadesmonsta/ddns_project:client-v${major}.${minor}.${patch}"
+docker tag "ghcr.io/hadesmonsta/ddns_project:client-latest" "ghcr.io/hadesmonsta/ddns_project:client-v${major}.${minor}"
+docker tag "ghcr.io/hadesmonsta/ddns_project:client-latest" "ghcr.io/hadesmonsta/ddns_project:client-v${major}"
+
+docker push "ghcr.io/hadesmonsta/ddns_project:client-v${major}.${minor}.${patch}"
+docker push "ghcr.io/hadesmonsta/ddns_project:client-v${major}.${minor}"
+docker push "ghcr.io/hadesmonsta/ddns_project:client-v${major}"
+docker push "ghcr.io/hadesmonsta/ddns_project:client-latest"
+
+##############
+### SERVER ###
+##############
 
 for tag_base in "server" "server-netcup"; do
-    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${full}" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}"
-    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${full}" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}.${minor}"
-    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${full}" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-latest"
+    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-latest" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}.${minor}.${patch}"
+    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-latest" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}.${minor}"
+    docker tag "ghcr.io/hadesmonsta/ddns_project:${tag_base}-latest" "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}"
 
-    docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${full}"
-    docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}"
+    docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}.${minor}.${patch}"
     docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}.${minor}"
+    docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-v${major}"
     docker push "ghcr.io/hadesmonsta/ddns_project:${tag_base}-latest"
 done
 
